021f74bfc8b54cf7b313c7808bc2096fb362772b
proRuslan77 <you@example.com>
2022-07-07 12:50:35 +0300

    add new create

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.header__btn').on('click', function () {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    })
+    $('.rightside-menu__close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    })
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    })
+})
