9082d9a2fadc65b33df8754c40a686c5a45a575b
proRuslan77 <you@example.com>
2022-07-18 15:16:37 +0300

    add last section on page contact

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,14 @@ $(function(){
         autoplay: true
     })
 
+    
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false
+    })
+
     var mixer = mixitup('.gallery__inner', {
         load : {
             filter: '.living'
